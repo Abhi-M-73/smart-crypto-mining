a928e0feb03303c739a892e661e8a4ff8e6b2aa7
Abhishek <abhishekmaddheshiya632@gmail.com>
2025-05-09 16:37:16 +0530

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div className='min-h-screen bg-color text-white'>
-      App
+    <div className='min-h-screen w-full bg-color'>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='max-w-7xl mx-auto bg-color text-white'>
+          Navbar
+    </div>
+  )
+}
+
+export default Navbar
